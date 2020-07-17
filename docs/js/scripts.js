666aefdf1dac8d4f2f932488b15d662ce266c0cb
Denis Snitko <denissnitko@gmail.com>
2020-07-17 19:53:00 +0300

    Add

---
+++ docs/js/scripts.js
@@ -0,0 +1,45 @@
+$(window).load(function(){
+	// Продукты
+	$('.info_deposit .slider').owlCarousel({
+		nav: true,
+		dots: false,
+		loop: false,
+		smartSpeed: 750,
+		mouseDrag: false,
+		onInitialized: function(event){
+			setHeight( $(event.target).find('.item_wrap') )
+		},
+		onResized: function(event){
+			$(event.target).find('.item_wrap').height('auto')
+
+			setHeight( $(event.target).find('.item_wrap') )
+		},
+		responsive: {
+			// breakpoint from 1101 up
+			1200 : {
+				items: 4,
+				margin: 30,
+			},
+			// breakpoint from 1024 up
+			1024 : {
+				items: 4,
+				margin: 15,
+			},
+			// breakpoint from 768 up
+			768 : {
+				items: 3,
+				margin: 15,
+			},
+			// breakpoint from 480 up
+			480 : {
+				items: 2,
+				margin: 15,
+			},
+			// breakpoint from 320 up
+			0 : {
+				items: 1,
+				margin: 15,
+			}
+		}
+	})
+})
